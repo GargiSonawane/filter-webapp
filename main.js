38423d16e3f385f94aa34a22b4ed44758433856d
Gargi Sonawane <67555203+GargiSonawane@users.noreply.github.com>
2021-06-23 11:36:32 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,21 @@
+noseX=0;
+noseY=0;
 function preload(){
 }
 
 function setup(){
     canvas = createCanvas(300, 300);
     canvas.center();
+    video=createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded(){
+    console.log("poseNet is initialized");
 }
 
 function draw(){
@@ -11,4 +23,16 @@ function draw(){
 
 function takeSnapshot(){
     save('myFilterImage.png');
+}
+
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results);
+        console.log("nose x = " + results[0].pose.nose.x);
+        console.log("nose y = " + results[0].pose.nose.y);
+        noseX=results[0].pose.nose.x-23;
+        noseY=results[0].pose.nose.y-20;
+        console.log("nose x = " + noseX);
+        console.log("nose y = " + noseY);
+    }
 }
